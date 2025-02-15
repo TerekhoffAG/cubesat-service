aae96e13f80f223e1ba33a10e91f130953f1bde4
Terekhoff Sasha <terekhoffag@gmail.com>
2025-02-15 17:30:55 +0400

    initial commit

---
+++ src/main/java/ru/alien/cubesat_service/CubesatServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.alien.cubesat_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubesatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubesatServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/alien/cubesat_service/CubesatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alien.cubesat_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubesatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
